a5541ba0657609617578c1ba0896377d666c2f7c
townsean <ashley.grenon@gmail.com>
2016-05-27 00:18:29 -0500

    fixed foot step audio

--- src/sprites/player.js
+++ src/sprites/player.js
@@ -6,6 +6,8 @@ export class Player extends Phaser.Sprite {
     constructor(game, x, y) {
         super(game, x, y, 'player');
         
+        this.footsteps = this.game.add.audio('footsteps');
+        
         this.game.physics.enable(this, Phaser.Physics.ARCADE);
                 
         this.body.drag.set(50);
@@ -15,17 +17,32 @@ export class Player extends Phaser.Sprite {
     
     move(cursors) {        
         if(cursors.up.isDown) {
-            this.game.physics.arcade.accelerationFromRotation(this.rotation, 150, this.body.acceleration);
+            this.game.physics.arcade.accelerationFromRotation(this.rotation, 25, this.body.acceleration);
+            
+            // play footsteps sound effect while walking
+            if(!this.footsteps.isPlaying) {  
+                this.footsteps.play();
+            }
         } else if(cursors.down.isDown) {       
-            this.game.physics.arcade.accelerationFromRotation(this.rotation, -150, this.body.acceleration);
+            this.game.physics.arcade.accelerationFromRotation(this.rotation, -25, this.body.acceleration);            
+            
+            // play footsteps sound effect while walking
+            if(!this.footsteps.isPlaying) {  
+                this.footsteps.play();
+            }
         } else {
-            this.body.acceleration.set(0);
+            this.body.acceleration.set(0);            
+            
+            // stop footsteps sound effect when movement stops
+            if(this.footsteps.isPlaying) {                
+                this.footsteps.stop();
+            }
         }     
         
         if(cursors.left.isDown) {
-            this.body.angularVelocity = -150;
+            this.body.angularVelocity = -50;
         } else if(cursors.right.isDown) {
-            this.body.angularVelocity = 150;
+            this.body.angularVelocity = 50;
         } else {
             this.body.angularVelocity = 0;
         }
