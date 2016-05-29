5d87a439eb0f65b79bf41102d2b7dccb39d8c987
nsipid <nsipid@gmail.com>
2016-05-29 14:15:37 -0500

    raycasting logic for tilemap

--- src/camera/camera.js
+++ src/camera/camera.js
@@ -1,29 +1,35 @@
 import { World } from "./world";
+import { Ray } from "./ray";
 import { Phaser } from "phaser";
 
 export class Camera {
-    constructor(bitmapData, world, fieldOfView, numberOfRays) {
+    constructor(canvas, world, fieldOfView, numberOfRays) {
         this.world = world;
-        this.bitmapData = bitmapData;
+        this.ctx = canvas.getContext('2d');
+        this.height = canvas.height;
+        this.width = canvas.width;
         this.fieldOfView = fieldOfView || 0.8;
-        this.scale = (bitmapData.width + bitmapData.height) / 1200;
-        this.columnWidth = bitmapData.width / numberOfRays;
+        this.scale = (this.width + this.height) / 1200;
+        this.columnWidth = this.width / numberOfRays;
+        this.numberOfRays = numberOfRays;
     }
     
-    render(sprite) {
-        this._drawColumns(sprite);
+    render(sprite) {     
+        this._drawColumns({x: this.world.convertWorldX(sprite.x), y: this.world.convertWorldY(sprite.y), direction: Phaser.Math.degToRad(sprite.angle)});
     }
     
-    _drawColumns(sprite) {    
-        this.bitmapData.ctx.save();
+    _drawColumns(player) {    
+        this.ctx.save();
+        this.ctx.fillStyle = '#000000';
+        this.ctx.fillRect(0, 0, this.width, this.height);
         for (let col = 0; col < this.numberOfRays; col++) {
             let x = col / this.numberOfRays - 0.5;
             let angle = Math.atan2(x, this.fieldOfView);
             
-            let ray = new Ray(sprite.x, sprite.y, angle, 14, this.world);
+            let ray = new Ray(player.x, player.y, player.direction + angle, 14, this.world);
             this._drawColumn(col, ray, angle);
         }
-        this.bitmapData.ctx.restore();
+        this.ctx.restore();
     }
     
     _drawColumn(column, ray, angle) {
@@ -33,21 +39,21 @@ export class Camera {
       
         while (++hit < ray.points.length && ray.points[hit].height <= 0);
         
-        for (let s = ray.point.length - 1; s >= 0; s--) {
-            let step = ray[s];
+        for (let s = ray.points.length - 1; s >= 0; s--) {
+            let step = ray.points[s];
             if (s === hit) {
-                let wallTexture = this.world.getWallTexture();
+                let wallTexture = this.world.getTexture(step.index);
                 let textureX = Math.floor(wallTexture.width * step.offset);
                 let projection = this._projectColumn(step.height, angle, step.distance);
-                this.bitmapData.ctx(wallTexture, textureX, 0, 1, wallTexture.height, left, projection.top, width, projection.height); 
+                this.ctx.drawImage(wallTexture, textureX, 0, 1, wallTexture.height, left, projection.top, width, projection.height); 
             }
         }
     }
     
     _projectColumn(height, angle, distance) {
         var z = distance * Math.cos(angle);
-        var wallHeight = this.bitmapData.height * height / z;
-        var bottom = this.bitmapData.height / 2 * (1 + 1 / z);
+        var wallHeight = this.height * height / z;
+        var bottom = this.height / 2 * (1 + 1 / z);
         return {
             top: bottom - wallHeight,
             height: wallHeight
--- src/camera/world.js
+++ src/camera/world.js
@@ -2,16 +2,54 @@ import { Ray } from './ray';
 import { Phaser } from 'phaser';
 
 export class World {
-    constructor(tileMap, wallTextureKey) {
+    constructor(tileMap) {
+        this.textureMap = {};
+        
         this.tileMap = tileMap;
-        this.wallTextureKey = wallTextureKey;
+        
+        let mapLength = this.tileMap.layers[0].data.length;
+        let mapWidth = this.tileMap.layers[0].data[0].length;
+        let array = [];
+        let mapArray = [];
+        for (let y = 0; y < mapLength; y++) {
+            array[y] = [];
+            mapArray[y] = [];
+            for (let x = 0; x < mapWidth; x++) {
+                array[y][x] = this.tileMap.layers[0].data[y][x].index;
+                if (array[y][x] < 0) {
+                    array[y][x] = 0;
+                }
+
+                mapArray[y].push(array[y][x]);
+                
+            }
+        }
+        
+        this.mapArray = mapArray;
     }
     
     getTile(x, y) {
-        return this.tileMap.layers[0].tilemap.getTile(x, y);
+        x = Math.floor(x);
+        y = Math.floor(y)
+        if (y < 0 || y > this.mapArray.length - 1 || x < 0 || x > this.mapArray[y].length - 1) {
+            return -1;
+        }
+        return this.mapArray[y][x];
     }
     
-    getWallTexture() {
-        return Phaser.Cache.getImage(this.wallTextureKey);
+    convertWorldX(x) {
+        return x / this.tileMap.tileWidth;
+    }
+    
+    convertWorldY(y) {
+        return y / this.tileMap.tileHeight;
+    }
+    
+    addTexture(index, image) {
+        this.textureMap[index] = image;
+    }
+    
+    getTexture(index) {
+        return this.textureMap[index];
     }
 }
